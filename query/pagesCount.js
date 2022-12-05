e706dd04281987b1a9b977a9c78be38a59c63a58
nihalDelimp <nihal@delimp.com>
2022-12-05 11:19:14 +0530

    New commit

--- query/pagesCount.js
+++
@@ -1,4 +0,0 @@
-export async function getPagesCount(posts, postsPerPage) {
-  const _postsPerPage = postsPerPage ?? (await getPostsPerPage());
-  return Math.ceil(posts.length / _postsPerPage);
-}
--- query/stickyPost.js
+++
@@ -1,3 +0,0 @@
-export function sortStickyPosts(posts) {
-  return [...posts].sort((post) => (post.isSticky ? -1 : 1));
-}

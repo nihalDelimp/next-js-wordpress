74cf38786e8f1cc38323ce07a64287b2ba4138e6
nihalDelimp <nihal@delimp.com>
2022-12-14 12:05:26 +0530

    Consultancy services

--- pages/consultation-services.js
+++ pages/consultation-services.js
@@ -1,16 +1,16 @@
-import getonsultationServicesData from "../query/services/consultationServices";
+import getConsultationServicesData from "../query/Services/consultationServices";
 import ConsultationServices from './../components/Services/ConsultationServices';
 
 const ConsultationServicesIndustry = ({ consultationServices }) => {
-     const resData = consultationServices.data.pageBy.servicesSection.servicesContent;
-     const section1 = resData && resData[0] ? resData[0] :  []
-     const section2 = resData && resData[1] ? resData[1] :  []
-     const section3 = resData && resData[2] ? resData[2] :  []
-     const section4 = resData && resData[3] ? resData[3] :  []
-     const section5 = resData && resData[4] ? resData[4] :  []
+    const resData = consultationServices.data.pageBy.servicesSection.servicesContent;
+    const section1 = resData && resData[0] ? resData[0] : []
+    const section2 = resData && resData[1] ? resData[1] : []
+    const section3 = resData && resData[2] ? resData[2] : []
+    const section4 = resData && resData[3] ? resData[3] : []
+    const section5 = resData && resData[4] ? resData[4] : []
 
     return (
-        <ConsultationServices section1={section1} section2={section2} section3={section3} section4={section4} section5 ={section5}  />
+        <ConsultationServices section1={section1} section2={section2} section3={section3} section4={section4} section5={section5} />
 
     );
 };
@@ -18,7 +18,7 @@ const ConsultationServicesIndustry = ({ consultationServices }) => {
 export async function getStaticProps(context) {
     return {
         props: {
-            consultationServices: await getonsultationServicesData()
+            consultationServices: await getConsultationServicesData()
 
         },
     };

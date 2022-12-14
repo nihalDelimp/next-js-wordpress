f00b7a6918b022f56492eb46cd690a1bdd1395eb
nihalDelimp <nihal@delimp.com>
2022-12-14 12:11:09 +0530

    Removing csss for _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
 // import "../styles/bootstrap.min.css";
-import "../styles/animate.min.css";
+// import "../styles/animate.min.css";
 // Global Style
-import "../styles/fullpage.css";
+// import "../styles/fullpage.css";
 // import "../styles/swiper.css";
-import "../styles/theme.css";
-import "../styles/landing-page.css";
+// import "../styles/theme.css";
+// import "../styles/landing-page.css";
 // import "../styles/all.min.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Header from "../components/Header/Header";
@@ -20,7 +20,7 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Header />
-			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+			{/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" /> */}
 			<ApolloProvider client={client}>
 				<Component {...pageProps} />
 			</ApolloProvider>
